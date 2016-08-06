4dea91e7f645e4305426396710384dd4fd80c4ce
Dan Jenner <danjenner@gmail.com>
2016-08-06 07:34:12 -0500

    Final version - added code comments, cleaned, fixed instructions

--- apps/app.js
+++ apps/app.js
@@ -1,6 +1,7 @@
+//Bing key so Cesium doesn't throw an error everytime it loads
 Cesium.BingMapsApi.defaultKey = 'ApcaGOvmoUuBrD2eHpVvRbOnrnJ8Fp33DrW8o6QLsJNZIpVHWGz9voYcM-BkEHYT';
 
-//new Cesium object, displays planet
+//new Cesium object, displays planet, generic zoom/display level
 var viewer = new Cesium.Viewer('cesiumContainer', {
     imageryProvider: new Cesium.ArcGisMapServerImageryProvider({
         url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer'
@@ -8,6 +9,7 @@ var viewer = new Cesium.Viewer('cesiumContainer', {
     baseLayerPicker: false
 });
 
+//build Ajax call, pull in start/end dates from form, gives an alert if no matches were found
 var getEQdata = function (quake) {
     var request = {
         format: 'geojson',
@@ -24,6 +26,7 @@ var getEQdata = function (quake) {
             if (result.features.length == 0) {
                 alert("There were no matches for your search. Try widening your search dates or lowering the magnitude.");
             } else {
+                //build viewer object from USGS return data
                 for (var i = 0; i < result.features.length; i++) {
                     var lat = result.features[i].geometry.coordinates[0];
                     var long = result.features[i].geometry.coordinates[1];
@@ -33,6 +36,7 @@ var getEQdata = function (quake) {
                     var material;
                     var date = new Date(result.features[i].properties.time);
 
+                    //set magnitude color and poly height based off magnitude
                     if (mag > 7) {
                         height = 1000000;
                         material = Cesium.Color.DARKRED.withAlpha(0.5);
@@ -54,9 +58,12 @@ var getEQdata = function (quake) {
                     } else if (mag > 1) {
                         height = 100000;
                         material = Cesium.Color.MEDIUMPURPLE.withAlpha(0.5);
+                    } else {
+                        height = 50000;
+                        material = Cesium.Color.RED.withAlpha(0.5);
                     }
 
-                    //creates a polygon, in this case, a rectangle, on position = 'fromDegrees'
+                    //creates a polygon, in this case, a square ("box"), on position 'fromDegrees', fills with material and height defined above
                     var shakeBox = viewer.entities.add({
                         name: 'Shake Viewer',
                         position: Cesium.Cartesian3.fromDegrees(lat, long),
@@ -64,7 +71,7 @@ var getEQdata = function (quake) {
                             '<p>Magnitude: ' + mag + '</p>' +
                             '<p>Date & Time: ' + date + '</p>',
                         box: {
-                            dimensions: new Cesium.Cartesian3(200000.0, 150000.0, height),
+                            dimensions: new Cesium.Cartesian3(150000.0, 150000.0, height),
                             //length, width and height
                             material: material,
                             //what fill color and transparency to use
@@ -95,7 +102,7 @@ quakeForm.prototype = {
     //collects the data from the form by id (matches above), get the value and adds to array
     collectFormData: function () {
         var output = {};
-        //for each of the input fields match the IDs and their values, and add them to the output array;
+        //for each of the input fields match the IDs and their values, and add them to the output array
         this.inputFieldIds.forEach(function (element) {
             output[element] = $('#' + element).val();
         });
@@ -128,6 +135,7 @@ $(document).ready(function () {
         timepicker: false
     });
 
+    //submit handling functions, get the form data, generate object and display
     $('.searchbox').submit(function (event) {
         viewer.dataSources.removeAll();
         event.preventDefault(event);
@@ -135,6 +143,7 @@ $(document).ready(function () {
         getEQdata(quake);
     });
 
+    //reload the form on click.  Unable to find alternate way (new box object) to better reset form after building the Cesium object
     $('.searchbox').on('click', '#reset', function (event) {
         event.preventDefault(event);
         location.reload();
